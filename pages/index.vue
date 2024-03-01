eca042b7acb565cc8b91bc8670e6a83f0da2b71a
Andres <andreees.ys@gmail.com>
2024-03-01 20:27:04 +0100

    TailwindCSS - Close #1

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,9 @@
 <template>
   <!--<h1>Index</h1>-->
   <!-- WELCOME -->
+  <h1 class="text-3xl font-bold text-blue-600 underline">
+    Hello world!
+  </h1>
   <!-- GALLERY -->
   <!-- FOOTER -->
   <Footer></Footer>
