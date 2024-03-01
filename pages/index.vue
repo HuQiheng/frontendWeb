a0398c54005504f3ecf071ac6c4f29876336e4e5
AndreiGabrielV <andreigabrielv@gmail.com>
2024-03-01 21:19:42 +0100

    Set up Prettier close #2

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,7 @@
 <template>
   <!--<h1>Index</h1>-->
   <!-- WELCOME -->
-  <h1 class="text-3xl font-bold text-blue-600 underline">
-    Hello world!
-  </h1>
+  <h1 class="text-3xl font-bold text-blue-600 underline">Hello world!</h1>
   <!-- GALLERY -->
   <!-- FOOTER -->
   <Footer></Footer>
