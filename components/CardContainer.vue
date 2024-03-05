276b1a029bb738e1110e2f9fd9f627e5336bb15a
AndreiGabrielV <andreigabrielv@gmail.com>
2024-03-05 11:46:05 +0100

    Fixed again again ⚒️

--- components/CardContainer.vue
+++ components/CardContainer.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="card-container overflow-x-auto flex justify-center py-4">
-    <Card v-for="(card, index) in cards" :key="index" :image="card.image" class="mr-8" />
+  <div class="card-container overflow-x-auto flex flex-row justify-center py-4">
+    <Card v-for="(card, index) in cards" :key="index" :image="card.image" class="mr-8 ml-8 mb-8" />
   </div>
 </template>
 
