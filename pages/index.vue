ffd10703a1a65671da59c0711e174f64c44bbc6d
AndreiGabrielV <andreigabrielv@gmail.com>
2024-03-05 11:07:56 +0100

    Fixed some stuff index page 🔧

--- pages/index.vue
+++ pages/index.vue
@@ -3,8 +3,8 @@
     <Title>WealthWars</Title>
     <Meta name="description" content="Un juego de estrategia donde tendrás que ganar dinero, erigir fábricas, y conquistar territorios para expandir tu influencia." />
   </Head>
-  <div class="flex flex-col justify-center items-center h-screen background mb-16">
-    <h1 class="text-8xl font-bold text-center mb-4">WealthWars</h1>
+  <div class="flex flex-col justify-center items-center h-screen background mb-16" style="max-width: 100%; padding: 0 20px;">
+    <h1 class="text-8xl font-bold text-center mb-4 max-w-xl">WealthWars</h1>
     <h2 class="max-w-2xl text-lg text-center mb-8">
       Un juego de estrategia donde tendrás que ganar dinero, erigir fábricas, y conquistar territorios para expandir tu influencia.
     </h2>
@@ -20,5 +20,7 @@
     background-image: url('/epic_background.jpg');
     background-size: cover;
     background-position: center;
+    height: 100vh;
+    width: 100%;
   }
 </style>
