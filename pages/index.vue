f1f9155e80cc8b3bf9a31aa22ac7e09cdc2ae3eb
Andres <andreees.ys@gmail.com>
2024-03-06 10:53:45 +0100

    Test

--- pages/index.vue
+++ pages/index.vue
@@ -4,13 +4,13 @@
     <Meta name="description" content="Un juego de estrategia donde tendrás que ganar dinero, erigir fábricas, y conquistar territorios para expandir tu influencia." />
   </Head>
   <div class="flex flex-col justify-center items-center h-screen w-full background mb-16">
-    <h1 class="xl:text-8xl text-4xl font-bold text-center mb-4">WealthWars</h1>
-    <h2 class="max-w-2xl text-sm xl:text-lg text-center mb-8">
+    <h1 class="text-4xl font-bold text-center mb-4">WealthWars</h1>
+    <h2 class="max-w-2xl text-sm text-center mb-8">
       Un juego de estrategia donde tendrás que ganar dinero, erigir fábricas, y conquistar territorios para expandir tu influencia.
     </h2>
     <NuxtLink to="/signin"><Button>JUGAR</Button></NuxtLink>
   </div>
-  <CardContainer class="flex flex-col xl:flex-row justify-center items-center"/>
+  <CardContainer class="flex flex-col justify-center items-center"/>
   <!-- FOOTER -->
   <Footer></Footer>
 </template>
