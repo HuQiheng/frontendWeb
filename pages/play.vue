35aa5e2cdbdc10444531bf0429f39f2ee0e20875
ayubero <andreees.ys@gmail.com>
2024-03-07 12:53:03 +0100

    board game

--- pages/play.vue
+++ pages/play.vue
@@ -1,1 +1,13 @@
-<template></template>
+<template>
+    <main class="w-full h-screen flex flex-row">
+        <!-- Game board -->
+        <section class="grow">
+            <!-- Map -->
+            <div></div>
+            <!-- Actions -->
+            <div></div>
+        </section>
+        <!-- Chat -->
+        <section class="w-96 border-l border-gray-300"></section>
+    </main>
+</template>
