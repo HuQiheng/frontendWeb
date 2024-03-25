95420c7236e56146c36fe79571fede8aa842ce37
ayubero <andreees.ys@gmail.com>
2024-03-25 08:26:32 +0100

    Callback for mobile frontend

---
+++ pages/auth.vue
@@ -0,0 +1,32 @@
+<template>
+    <Head>
+      <Title>Iniciar sesión | WealthWars</Title>
+      <Meta name="description" content="Inicio de sesión para jugar WealthWars." />
+    </Head>
+    <Header></Header>
+    <Main>
+      <h1 class="my-4 text-2xl">Iniciar sesión</h1>
+      <GoogleSignInButton @success="handleLoginSuccess" @error="handleLoginError"></GoogleSignInButton>
+    </Main>
+    <Footer></Footer>
+  </template>
+  
+  <script setup lang="ts">
+    import { GoogleSignInButton, type CredentialResponse } from 'vue3-google-signin';
+  
+    // Handle success event
+    const handleLoginSuccess = (response: CredentialResponse) => {
+      const { credential } = response;
+
+      // Send result back to the app
+      window.location.href = "app://signinresult?token=" + encodeURIComponent(
+        JSON.stringify(credential)
+      );
+    };
+  
+    // Handle an error event
+    const handleLoginError = () => {
+      console.error('Login failed');
+    };
+  </script>
+  
