70be7504d016b280db2356e4fabbf0c005ba4cb8
Andres <andreees.ys@gmail.com>
2024-05-07 23:04:40 +0200

    Badges & Style changes

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -8,11 +8,11 @@
   </Head>
   <Notification ref="notification" />
   <GameInvitation ref="invitation" @accept="handleAccept" />
-  <main class="w-full h-screen flex flex-col">
+  <main class="w-full h-screen flex flex-col bg-primary">
     <!-- Top section -->
     <section class="flex-1 justify-center items-center p-6">
       <!-- section class="flex flex-col justify-center items-center" last -->
-      <div class="rounded-xl border shadow-md p-6">
+      <div class="rounded-xl border border-primary-dark shadow-md p-6">
         <!-- User's profile settings -->
         <div class="top-14 flex flex-col justify-center items-center">
           <img
@@ -43,7 +43,7 @@
     <section class="flex-1 flex flex-col xl:flex-row">
       <!-- Left side, match related-->
       <div class="flex-1 p-6">
-        <div class="flex flex-col w-full h-full rounded-xl border shadow-md p-6">
+        <div class="flex flex-col w-full h-full rounded-xl border border-primary-dark shadow-md p-6">
           <h2 class="flex text-2xl font-bold m-4 text-center justify-center w-full">Juego</h2>
           <hr />
           <!-- Start new match -->
@@ -69,7 +69,7 @@
       </div>
       <!-- Right side, friends related-->
       <div class="flex-1 p-6">
-        <div class="flex flex-col h-full rounded-xl border shadow-md p-6 w-full">
+        <div class="flex flex-col h-full rounded-xl border border-primary-dark shadow-md p-6 w-full">
           <!-- Title -->
           <h2 class="text-2xl font-bold m-4 text-center">Lista de amigos</h2>
           <hr />
